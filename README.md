d1a4ef12556e947b29c4652ada785b2e1e15db42
Filippo Bragato <filo.bragato@gmail.com>
2023-03-01 09:43:40 +0100

    Dataset api

--- README.md
+++ README.md
@@ -11,3 +11,111 @@ The aim of this project is studying the correlation betweeen sensors data in ord
     - …
 4. Implementare un sottoinsieme delle tecniche di correlazione individuate al punto precedente [deadline 21/04]
 5. Studio della correlazione, ottenendo risultati numerici che possano essere poi usati per il framework PQoS [deadline 05/05]
+
+## SELMA dataset study
+
+The dataset provides 216 settings whose parameters are:
+- 3 daytime (Night, Noon, Sunset)
+- 9 weather conditions (Clear, Cloudy, HardFog, HardRain, MidFog, MidRain, SoftRain, Wet, WetCloudy)
+- 8 towns
+
+Each acquisition is made by:
+- 7 rgb cameras ( $90\deg$ horizontal FoV, res $1280 \times 640$)
+- 7 depth cameras ( $90\deg$ horizontal FoV, res $1280 \times 640$)
+- 7 semantic cameras ( $90\deg$ horizontal FoV, res $1280 \times 640$)
+- 3 semantic LiDARs ( $64\deg$ verical channels, $100 000$ points per second, range $100\mathsf{m}$)
+
+The dataset provides the ground truth for $36$ distinct classes 
+
+## Dependencies
+    anytree                   2.8.0                    pypi_0    pypi
+    asttokens                 2.2.1              pyhd8ed1ab_0    conda-forge      
+    attrs                     22.2.0                   pypi_0    pypi
+    autopep8                  1.6.0              pyhd3eb1b0_1
+    backcall                  0.2.0              pyh9f0ad1d_0    conda-forge      
+    backports                 1.0                pyhd8ed1ab_3    conda-forge      
+    backports.functools_lru_cache 1.6.4              pyhd8ed1ab_0    conda-forge
+    ca-certificates           2023.01.10           haa95532_0
+    certifi                   2022.12.7        py38haa95532_0
+    click                     8.1.3                    pypi_0    pypi
+    colorama                  0.4.6              pyhd8ed1ab_0    conda-forge
+    configargparse            1.5.3                    pypi_0    pypi
+    contourpy                 1.0.7                    pypi_0    pypi
+    cycler                    0.11.0                   pypi_0    pypi
+    dash                      2.8.1                    pypi_0    pypi
+    dash-core-components      2.0.0                    pypi_0    pypi
+    dash-html-components      2.0.0                    pypi_0    pypi
+    dash-table                5.0.0                    pypi_0    pypi
+    debugpy                   1.5.1            py38hd77b12b_0
+    decorator                 5.1.1              pyhd8ed1ab_0    conda-forge
+    executing                 1.2.0              pyhd8ed1ab_0    conda-forge
+    fastjsonschema            2.16.3                   pypi_0    pypi
+    flask                     2.2.3                    pypi_0    pypi
+    fonttools                 4.38.0                   pypi_0    pypi
+    importlib-metadata        6.0.0              pyha770c72_0    conda-forge
+    importlib-resources       5.12.0                   pypi_0    pypi
+    importlib_metadata        6.0.0                hd8ed1ab_0    conda-forge
+    ipykernel                 6.15.0             pyh025b116_0    conda-forge
+    ipython                   8.10.0             pyh08f2357_0    conda-forge
+    ipywidgets                8.0.4                    pypi_0    pypi
+    itsdangerous              2.1.2                    pypi_0    pypi
+    jedi                      0.18.2             pyhd8ed1ab_0    conda-forge
+    jinja2                    3.1.2                    pypi_0    pypi
+    jsonschema                4.17.3                   pypi_0    pypi
+    jupyter_client            8.0.3              pyhd8ed1ab_0    conda-forge
+    jupyter_core              5.2.0            py38haa244fe_0    conda-forge
+    jupyterlab-widgets        3.0.5                    pypi_0    pypi
+    kiwisolver                1.4.4                    pypi_0    pypi
+    libffi                    3.4.2                hd77b12b_6
+    libsodium                 1.0.18               h8d14728_1    conda-forge
+    markupsafe                2.1.2                    pypi_0    pypi
+    matplotlib                3.7.0                    pypi_0    pypi
+    matplotlib-inline         0.1.6              pyhd8ed1ab_0    conda-forge
+    nbformat                  5.5.0                    pypi_0    pypi
+    nest-asyncio              1.5.6              pyhd8ed1ab_0    conda-forge
+    numpy                     1.24.2                   pypi_0    pypi
+    open3d                    0.16.0                   pypi_0    pypi
+    openssl                   1.1.1t               h2bbff1b_0
+    packaging                 23.0               pyhd8ed1ab_0    conda-forge
+    pandas                    1.5.3                    pypi_0    pypi
+    parso                     0.8.3              pyhd8ed1ab_0    conda-forge
+    pickleshare               0.7.5                   py_1003    conda-forge
+    pillow                    9.4.0                    pypi_0    pypi
+    pip                       22.3.1           py38haa95532_0
+    pkgutil-resolve-name      1.3.10                   pypi_0    pypi
+    platformdirs              3.0.0              pyhd8ed1ab_0    conda-forge
+    plotly                    5.13.1                   pypi_0    pypi
+    plyfile                   0.7.4                    pypi_0    pypi
+    prompt-toolkit            3.0.36             pyha770c72_0    conda-forge
+    psutil                    5.9.0            py38h2bbff1b_0
+    pure_eval                 0.2.2              pyhd8ed1ab_0    conda-forge
+    pycodestyle               2.10.0           py38haa95532_0
+    pygments                  2.14.0             pyhd8ed1ab_0    conda-forge
+    pyparsing                 3.0.9                    pypi_0    pypi
+    pyrsistent                0.19.3                   pypi_0    pypi
+    python                    3.8.16               h6244533_2
+    python-dateutil           2.8.2              pyhd8ed1ab_0    conda-forge
+    python_abi                3.8                      2_cp38    conda-forge
+    pytz                      2022.7.1                 pypi_0    pypi
+    pywin32                   227              py38h294d835_1    conda-forge
+    pyzmq                     23.2.0           py38hd77b12b_0
+    seaborn                   0.12.2                   pypi_0    pypi
+    setuptools                65.6.3           py38haa95532_0
+    six                       1.16.0             pyh6c4a22f_0    conda-forge
+    sqlite                    3.40.1               h2bbff1b_0
+    stack_data                0.6.2              pyhd8ed1ab_0    conda-forge
+    tenacity                  8.2.2                    pypi_0    pypi
+    toml                      0.10.2             pyhd3eb1b0_0
+    tornado                   6.2              py38h294d835_0    conda-forge
+    traitlets                 5.9.0              pyhd8ed1ab_0    conda-forge
+    typing-extensions         4.4.0                hd8ed1ab_0    conda-forge
+    typing_extensions         4.4.0              pyha770c72_0    conda-forge
+    vc                        14.2                 h21ff451_1
+    vs2015_runtime            14.27.29016          h5e58377_2
+    wcwidth                   0.2.6              pyhd8ed1ab_0    conda-forge
+    werkzeug                  2.2.3                    pypi_0    pypi
+    wheel                     0.38.4           py38haa95532_0
+    widgetsnbextension        4.0.5                    pypi_0    pypi
+    wincertstore              0.2              py38haa95532_2
+    zeromq                    4.3.4                h0e60522_1    conda-forge
+    zipp                      3.15.0             pyhd8ed1ab_0    conda-forge
---
+++ SELMA/api.py
@@ -0,0 +1,226 @@
+import os
+from enum import Enum
+import open3d as o3d
+import seaborn as sns
+from plyfile import PlyData
+import numpy as np
+
+class Town(Enum):
+    """Town available in the dataset
+
+    Args:
+        Enum (str): name of the town
+    """
+    T1 = "Town01"
+    T2 = "Town02"
+    T3 = "Town03"
+    T4 = "Town04"
+    T5 = "Town05"
+    T6 = "Town06"
+    T7 = "Town07"
+    T10 = "Town10HD"
+
+
+class Weather(Enum):
+    """weather available in the dataset
+
+    Args:
+        Enum (str): description of the weather
+    """
+    Clear = "Clear"
+    Cloudy = "Cloudy"
+    HardFog = "HardFog"
+    HardRain = "HardRain"
+    MidFog = "MidFog"
+    MidRain = "MidRain"
+    SoftRain = "SoftRain"
+    Wet = "Wet"
+    WetCloudy = "WetCloudy"
+
+
+class Time(Enum):
+    """Time available in the dataset
+
+    Args:
+        Enum (str): time moment
+    """
+    Night = "Night"
+    Noon = "Noon"
+    Sunset = "Sunset"
+
+
+class Sensor(Enum):
+    """Sensor available in the dataset
+
+    Args:
+        Enum (str): name and position of the sensor
+    """
+    CB = "CAM_BACK"
+    CD = "CAM_DESK"
+    CF = "CAM_FRONT"
+    CFL = "CAM_FRONT_LEFT"
+    CFR = "CAM_FRONT_RIGHT"
+    CL = "CAM_LEFT"
+    CR = "CAM_RIGHT"
+    DB = "DEPTHCAM_BACK"
+    DD = "DEPTHCAM_DESK"
+    DF = "DEPTHCAM_FRONT"
+    DFL = "DEPTHCAM_FRONT_LEFT"
+    DFR = "DEPTHCAM_FRONT_RIGHT"
+    DL = "DEPTHCAM_LEFT"
+    DR = "DEPTHCAM_RIGHT"
+    LFL = "LIDAR_FRONT_LEFT"
+    LFR = "LIDAR_FRONT_RIGHT"
+    LT = "LIDAR_TOP"
+    SB = "SEGCAM_BACK"
+    SD = "SEGCAM_DESK"
+    SF = "SEGCAM_FRONT"
+    SFL = "SEGCAM_FRONT_LEFT"
+    SFR = "SEGCAM_FRONT_RIGHT"
+    SL = "SEGCAM_LEFT"
+    SR = "SEGCAM_RIGHT"
+
+    def getEstention(self):
+        """get the estention of the stored file according to the sensor
+
+        Returns:
+            str: estention of the file representing those objects
+        """
+        if self.value.find("CAM_") == -1:
+            return ".ply"
+        else:
+            return ".jpg"
+
+
+class Dataset:
+
+    def __init__(self, root: str):
+        """Initializes the dataset
+
+        Args:
+            root (str): path to the root
+        """
+        self.root_path = root
+
+    def _get_path_folder(self, scope: str, town: Town, weather: Weather, time: Time, sensor: Sensor):
+        """Get the folder where the specified data are stored
+
+        Args:
+            scope (str): scope of the dataset can be CV or ...
+            town (Town): The town in which the simulaiton has been performed
+            weather (Weather): The wether with which the simulaiton has been performed
+            time (Time): The time in which the simulaiton has been performed
+            sensor (Sensor): The sensor mesuring in the simulaiton
+
+        Returns:
+            str: path to the desired folder
+        """
+        return os.path.join(self.root_path,
+                            scope,
+                            "dataset",
+                            town.value+"_Opt_"+weather.value+time.value,
+                            sensor.value)
+
+    def _get_path_file(self, scope: str, town: Town, weather: Weather, time: Time, sensor: Sensor, id: int):
+        """Get the path where the desired data are stored
+
+        Args:
+            scope (str): scope of the dataset can be CV or ...
+            town (Town): The town in which the simulaiton has been performed
+            weather (Weather): The wether with which the simulaiton has been performed
+            time (Time): The time in which the simulaiton has been performed
+            sensor (Sensor): The sensor mesuring in the simulaiton
+            id (int): the ID of the sample
+
+        Returns:
+            str: path to the desired sample
+        """
+        return os.path.join(self._get_path_folder(scope, town, weather, time, sensor),
+                            town.value+"_Opt_"+weather.value+time.value+"_"+str(id)+sensor.getEstention())
+
+    def get_ids(self, scope: str, town: Town, weather: Weather, time: Time, sensor: Sensor):
+        """get all the IDs of a specific simulation
+
+        Args:
+            scope (str): scope of the dataset can be CV or ...
+            town (Town): The town in which the simulaiton has been performed
+            weather (Weather): The wether with which the simulaiton has been performed
+            time (Time): The time in which the simulaiton has been performed
+            sensor (Sensor): The sensor mesuring in the simulaiton
+
+        Returns:
+            list(int): list of all the available samples
+        """
+        folder = self._get_path_folder(scope, town, weather, time, sensor)
+        folder_content = os.listdir(folder)
+        folder_content = [file for file in folder_content if file.endswith(sensor.getEstention())]
+        ids = [int(file.split('_')[3][:-4]) for file in folder_content]
+        return ids
+    
+    def open_sample(self, scope: str, town: Town, weather: Weather, time: Time, sensor: Sensor, id:int):
+        """returns the value of the selected sample
+
+        Args:
+            scope (str): scope of the dataset can be CV or ...
+            town (Town): The town in which the simulaiton has been performed
+            weather (Weather): The wether with which the simulaiton has been performed
+            time (Time): The time in which the simulaiton has been performed
+            sensor (Sensor): The sensor mesuring in the simulaiton
+            id (int): the ID of the sample
+
+        Raises:
+            ValueError: if the function does not know how to handle the format
+
+        Returns:
+            dict: with keys ["type", "data", "class"] containing respectively the type of the data, the data itself and the ground truth for each sample in data
+        """
+        file_path = self._get_path_file(scope, town, weather, time, sensor, id)
+        out = {}
+        if (sensor.getEstention() == ".ply"):
+            out["type"] = "PointCloud"
+            data = PlyData.read(file_path)
+            points = [data['vertex'][axis] for axis in ['x', 'y', 'z']]
+            points = np.array(points).T
+            objTag = data['vertex']['ObjTag']
+            out["data"] = points
+            out["class"] = objTag
+        elif (sensor.getEstention() == ".jpg"):
+            out["type"] = "Image"
+            raise ValueError("Implementami")
+        else:
+            raise ValueError("Unkown file estention")
+        return out
+
+    def visualize_sample(self, sample:dict):
+        """visualize the sample
+
+        Args:
+            sample (dict): the sample to visualize 
+
+        Raises:
+            ValueError: if the function does not know how to handle the format
+        """
+        
+        if (sample["type"] == "PointCloud"):
+
+            palette = sns.color_palette("hsv", n_colors=36)
+            get_color = lambda tag:palette[tag%36]
+            colors = np.array(np.vectorize(get_color)(sample["class"])).T
+
+            pcd = o3d.geometry.PointCloud()
+            pcd.points = o3d.utility.Vector3dVector(sample["data"])
+            pcd.colors = o3d.utility.Vector3dVector(colors)
+
+            vis = o3d.visualization.Visualizer()
+            vis.create_window()
+            vis.add_geometry(pcd)
+            vis.get_render_option().background_color = np.asarray([0, 0, 0])
+            vis.run()
+            vis.destroy_window()
+
+        elif (sample["type"] == "PointCloud"):
+            raise ValueError("Implementami")
+        else:
+            raise ValueError("Unkown file estention")
+        
+    
